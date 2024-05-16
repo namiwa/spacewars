4150a276d1019315f7ccfb5c8ecc0676ba2f9034
Khairul Iman <42546125+namiwa@users.noreply.github.com>
2024-05-17 01:56:10 +0800

    deps: added glfw and its linked library to zig

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const gl = @cImport(@cInclude("glad/gl.h"));
-
+const glfw = @cImport(@cInclude("GLFW/glfw3.h"));
 pub fn main() !u8 {
-    if (!builtin.is_test) {
-        std.debug.print("zig-game started!\n", .{}); 
-        std.debug.print("testing glad version: {d}\n", .{gl.GLAD_GL_VERSION_4_6});
+    std.debug.print("zig-game started!\n", .{}); 
+    std.debug.print("testing glad version: {d}\n", .{gl.GLAD_GL_VERSION_4_6});
+    const ok = glfw.glfwInit();
+    if (ok == 0) {
+        std.debug.panic("GLFW failed to init\n", .{});
     }
+    std.debug.print("state of ok is : {d}\n", .{ok});
+    defer glfw.glfwTerminate();
     return 0;
 }
 
